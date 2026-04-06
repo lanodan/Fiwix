13476710706c0705e0b2cbeed3705b93b85b691a
Jordi Sanfeliu <jordi@fibranet.cat>
2026-04-06 09:08:06 +0200

    added defined constants for mailboxes and for debug purposes in 'lwipopts.h'

--- net/lwip/src/include/lwipopts.h
+++ net/lwip/src/include/lwipopts.h
@@ -1,5 +1,7 @@
 #include <fiwix/mm.h>
 
+#undef NO_SYS
+
 /* Use the existing allocator. */
 #define MEM_CUSTOM_ALLOCATOR 1
 #define MEM_CUSTOM_FREE(x) kfree((unsigned int)x)
@@ -13,6 +15,11 @@
 #define FD_ISSET __FD_ISSET
 
 #define ptrdiff_t unsigned int
+#define TCPIP_MBOX_SIZE	10
+#define DEFAULT_RAW_RECVMBOX_SIZE 10
+#define DEFAULT_UDP_RECVMBOX_SIZE 10
+#define DEFAULT_TCP_RECVMBOX_SIZE 10
+#define DEFAULT_ACCEPTMBOX_SIZE 10
 
 /* Enable parts of lwIP */
 #define LWIP_ARP 1
@@ -29,6 +36,42 @@
 #define LWIP_IPV6 0
 #define LWIP_IPV6_DHCP6 0
 #define LWIP_RAW 1
+
+/* debug */
+/*
+#define LWIP_DEBUG 1
+#define LWIP_DBG_MIN_LEVEL LWIP_DBG_LEVEL_ALL
+
+#define NETIF_DEBUG LWIP_DBG_ON
+#define PBUF_DEBUG LWIP_DBG_ON
+#define API_LIB_DEBUG LWIP_DBG_ON
+#define API_MSG_DEBUG LWIP_DBG_ON
+#define SOCKETS_DEBUG LWIP_DBG_ON
+#define ICMP_DEBUG LWIP_DBG_ON
+#define INET_DEBUG LWIP_DBG_ON
+#define IP_DEBUG LWIP_DBG_ON
+#define RAW_DEBUG LWIP_DBG_ON
+#define MEM_DEBUG LWIP_DBG_ON
+#define MEMP_DEBUG LWIP_DBG_ON
+#define SYS_DEBUG LWIP_DBG_ON
+#define TIMERS_DEBUG LWIP_DBG_ON
+*/
+/*
+#define TCP_DEBUG LWIP_DBG_ON
+#define TCP_INPUT_DEBUG LWIP_DBG_ON
+#define TCP_FR_DEBUG LWIP_DBG_ON
+#define TCP_RTO_DEBUG LWIP_DBG_ON
+#define TCP_CWND_DEBUG LWIP_DBG_ON
+#define TCP_WND_DEBUG LWIP_DBG_ON
+#define TCP_OUTPUT_DEBUG LWIP_DBG_ON
+#define TCP_RST_DEBUG LWIP_DBG_ON
+#define TCP_QLEN_DEBUG LWIP_DBG_ON
+#define UDP_DEBUG LWIP_DBG_ON
+#define TCPIP_DEBUG LWIP_DBG_ON
+#define DHCP_DEBUG LWIP_DBG_ON
+#define AUTOIP_DEBUG LWIP_DBG_ON
+#define DNS_DEBUG LWIP_DBG_ON
+*/
 
 #define SA_FAMILY_T_DEFINED 1
 
